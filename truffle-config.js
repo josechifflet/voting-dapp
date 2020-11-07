3a6114cbcd738d2607c2416d463a72abcf71219c
JosePedro <josepedro@decemberlabs.com>
2020-11-07 15:46:48 -0300

    refactor proyect

--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      port: 8545,
+      network_id: "*", // Match any network id
     },
-    develop: {
-      port: 8545
-    }
-  }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.20", // ex:  "0.4.20". (Default: Truffle's installed solc)
+    },
+  },
 };
