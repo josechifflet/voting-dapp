b4c36dcae889f84ab8ae4b239dbd9c7894175f2b
Juan Pablo Zufriategui <juanpablo@biller.uy>
2020-11-14 12:18:49 -0300

    Registro de los votantes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -21,9 +21,17 @@ contract Election {
         uint indexed _candidateId
     );
 
+    //Voters register
+    mapping(address => bool) public votersRegister;
+
+    //Admins register
+    mapping(address => bool) private adminsRegister; //Los que pueden llamar a registerVoters
+
     function Election () public {
         addCandidate("Donald Trump");
         addCandidate("Joe Biden");
+        // adminsRegister[0x3d9671839c2db7091Aa58178f05aD258bF339E86] = true;
+        // votersRegister[0x02DC2AF71D0198EED253184903A4B429050828D0] = true;
     }
 
     function addCandidate (string _name) private {
@@ -31,9 +39,17 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function registerVoters (address _address) public {
+        // require(adminsRegister[msg.sender]);
+        votersRegister[_address] = true;
+    }
+
     function vote (uint _candidateId) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
+
+        // require that voter is registered
+        require(votersRegister[msg.sender]);
 
         // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
